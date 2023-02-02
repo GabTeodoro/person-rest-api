98288f8e1ed2303ba94c5c202ad1826a4f25620d
Gabriel Teodoro <teodoro.gt@hotmail.com>
2023-02-02 11:05:09 -0300

    Developed list all people

--- src/main/java/com/study/personapi/controller/PersonController.java
+++ src/main/java/com/study/personapi/controller/PersonController.java
@@ -1,11 +1,13 @@
 package com.study.personapi.controller;
 
 import com.study.personapi.dto.MessageResponseDTO;
-import com.study.personapi.entity.Person;
+import com.study.personapi.dto.request.PersonDTO;
 import com.study.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -20,7 +22,12 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    private MessageResponseDTO createPerson(@RequestBody Person person) {
-        return service.createPerson(person);
+    private MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return service.createPerson(personDTO);
+    }
+
+    @GetMapping
+    private List<PersonDTO> listAll() {
+       return service.listAll();
     }
 }
--- src/main/java/com/study/personapi/service/PersonService.java
+++ src/main/java/com/study/personapi/service/PersonService.java
@@ -1,27 +1,40 @@
 package com.study.personapi.service;
 
 import com.study.personapi.dto.MessageResponseDTO;
+import com.study.personapi.dto.request.PersonDTO;
 import com.study.personapi.entity.Person;
+import com.study.personapi.mapper.PersonMapper;
 import com.study.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
 
-    private PersonRepository repository;
+    private final PersonRepository repository;
+    private final PersonMapper mapper;
 
     @Autowired
-    public PersonService(PersonRepository repository) {
+    public PersonService(PersonRepository repository, PersonMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
-
-    public MessageResponseDTO createPerson(Person person) {
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person person = mapper.toModel(personDTO);
         Person savedPerson = repository.save(person);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+
+        List<Person> people = repository.findAll();
+        return people.stream().map(mapper::toDTO).collect(Collectors.toList());
     }
 }
